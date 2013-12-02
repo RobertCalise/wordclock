0fb9e2281bc58c60d33587c387e232ac2ccf8ae5
Robert Calise <bob@theyec.org>
2013-12-02 15:37:47 -0500

    Initial version

---
+++ clock.js
@@ -0,0 +1,14 @@
+function wordclock() {
+  var date = new Date;
+  var mins = date.getMinutes();	
+  var rmin = 5 * Math.round(mins/5); // Round to nearest 5 minutes.
+  var hour = (mins <= 30) ? date.getHours() : date.getHours() + 1;	
+  var desc = (mins <= 30) ? "past" : "to";
+  var xmin = (mins <= 30) ? rmin : (60 - rmin);
+  jQuery.when(jQuery(".active").removeClass("active")).then(function() {
+    jQuery(".min"+xmin+", .hr"+hour+", ."+desc).addClass("active");		
+  });		
+}
+
+wordclock();
+setInterval(function() { wordclock(); }, 1000);
